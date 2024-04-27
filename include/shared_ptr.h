ee96712bfb4ec72362a99f4b1552181ae760b6d6
soundofautumn <qjm1412@163.com>
2024-04-27 16:02:23 +0800

    init commit

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -1,5 +1,168 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
 
+template<typename T>
+class SharedPtr {
+private:
+    T *_p;
+    size_t *_count;
+public:
+    SharedPtr();
+
+    explicit SharedPtr(T *p);
+
+    SharedPtr(const SharedPtr &ptr);
+
+    SharedPtr(SharedPtr &&ptr) noexcept;
+
+    ~SharedPtr();
+
+    SharedPtr &operator=(const SharedPtr &ptr);
+
+    SharedPtr &operator=(SharedPtr &&ptr) noexcept;
+
+    T *get() const;
+
+    void reset();
+
+    void reset(T *p);
+
+    [[nodiscard]] size_t use_count() const;
+
+    T &operator*() const;
+
+    T *operator->() const;
+
+    explicit operator bool() const;
+};
+
+template<typename T, typename... Args>
+SharedPtr<T> make_shared(Args &&... args) {
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
+
+
+template<typename T>
+SharedPtr<T>::SharedPtr() : _p(nullptr), _count(nullptr) {
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(T *p) : _p(p), _count(new size_t(1)) {
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr &ptr) : _p(ptr._p), _count(ptr._count) {
+    if (_count) {
+        ++(*_count);
+    }
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(SharedPtr &&ptr) noexcept : _p(ptr._p), _count(ptr._count) {
+    ptr._p = nullptr;
+    ptr._count = nullptr;
+}
+
+template<typename T>
+SharedPtr<T>::~SharedPtr() {
+    if (_count) {
+        --(*_count);
+        if (*_count == 0) {
+            delete _p;
+            delete _count;
+            _p = nullptr;
+            _count = nullptr;
+        }
+    }
+}
+
+template<typename T>
+SharedPtr<T> &SharedPtr<T>::operator=(const SharedPtr &ptr) {
+    if (this != &ptr) {
+        if (_count) {
+            --(*_count);
+            if (*_count == 0) {
+                delete _p;
+                delete _count;
+            }
+        }
+        _p = ptr._p;
+        _count = ptr._count;
+        if (_count) {
+            ++(*_count);
+        }
+    }
+    return *this;
+}
+
+template<typename T>
+SharedPtr<T> &SharedPtr<T>::operator=(SharedPtr &&ptr) noexcept {
+    if (this != &ptr) {
+        if (_count) {
+            --(*_count);
+            if (*_count == 0) {
+                delete _p;
+                delete _count;
+            }
+        }
+        _p = ptr._p;
+        _count = ptr._count;
+        ptr._p = nullptr;
+        ptr._count = nullptr;
+    }
+    return *this;
+}
+
+template<typename T>
+T *SharedPtr<T>::get() const {
+    return _p;
+}
+
+template<typename T>
+void SharedPtr<T>::reset() {
+    if (_count) {
+        --(*_count);
+        if (*_count == 0) {
+            delete _p;
+            delete _count;
+        }
+    }
+    _p = nullptr;
+    _count = nullptr;
+}
+
+template<typename T>
+void SharedPtr<T>::reset(T *p) {
+    if (_count) {
+        --(*_count);
+        if (*_count == 0) {
+            delete _p;
+            delete _count;
+        }
+    }
+    _p = p;
+    _count = new size_t(1);
+}
+
+template<typename T>
+size_t SharedPtr<T>::use_count() const {
+    return _count ? *_count : 0;
+}
+
+template<typename T>
+T *SharedPtr<T>::operator->() const {
+    return _p;
+}
+
+template<typename T>
+T &SharedPtr<T>::operator*() const {
+    return *_p;
+}
+
+template<typename T>
+SharedPtr<T>::operator bool() const {
+    return _p != nullptr;
+}
+
 
 #endif //SHARED_PTR
--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,14 +1,116 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
 
+template<typename T>
+class UniquePtr {
+
+private:
+    T *_p;
+
+public:
+    UniquePtr();
+
+    explicit UniquePtr(T *p);
+
+    UniquePtr(const UniquePtr &ptr) = delete;
+
+    UniquePtr(UniquePtr &&ptr) noexcept;
+
+    ~UniquePtr();
+
+    UniquePtr &operator=(const UniquePtr &ptr) = delete;
+
+    UniquePtr &operator=(UniquePtr &&ptr) noexcept;
+
+    T *get() const;
+
+    T *release();
+
+    void reset();
+
+    void reset(T *p);
+
+    T &operator*() const;
+
+    T *operator->() const;
+
+    explicit operator bool() const;
+
+};
 
 
+template<typename T, typename... Args>
+UniquePtr<T> make_unique(Args &&... args) {
+    return UniquePtr<T>(new T(std::forward<Args>(args)...));
+}
 
 
+template<typename T>
+UniquePtr<T>::UniquePtr() : _p(nullptr) {
+}
 
+template<typename T>
+UniquePtr<T>::UniquePtr(T *p) : _p(p) {
+}
 
+template<typename T>
+UniquePtr<T>::UniquePtr(UniquePtr &&ptr) noexcept : _p(ptr._p) {
+    ptr._p = nullptr;
+}
 
+template<typename T>
+T *UniquePtr<T>::get() const {
+    return _p;
+}
 
+template<typename T>
+T *UniquePtr<T>::release() {
+    T *temp = _p;
+    _p = nullptr;
+    return temp;
+}
+
+template<typename T>
+void UniquePtr<T>::reset() {
+    delete _p;
+    _p = nullptr;
+}
+
+template<typename T>
+void UniquePtr<T>::reset(T *p) {
+    delete _p;
+    _p = p;
+}
+
+template<typename T>
+T &UniquePtr<T>::operator*() const {
+    return *_p;
+}
+
+template<typename T>
+T *UniquePtr<T>::operator->() const {
+    return _p;
+}
+
+template<typename T>
+UniquePtr<T>::operator bool() const {
+    return _p != nullptr;
+}
+
+template<typename T>
+UniquePtr<T> &UniquePtr<T>::operator=(UniquePtr &&ptr) noexcept {
+    if (this != &ptr) {
+        delete _p;
+        _p = ptr._p;
+        ptr._p = nullptr;
+    }
+    return *this;
+}
+
+template<typename T>
+UniquePtr<T>::~UniquePtr() {
+    delete _p;
+}
 
 
 #endif //UNIQUE_PTR
